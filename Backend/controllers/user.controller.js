4a3b7666476756225d5457203a62108078965a88
Arkopravo Ghosh <arkopravoghosh789.com>
2024-12-03 21:54:14 +0530

    ....

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -11,6 +11,11 @@ module.exports.registerUser = async (req, res, next) => {
     }
     const {fullname, email, password} = req.body;
     // console.log(req.body)
+    const isUserAlready = await userModel.findOne({ email });
+
+    if (isUserAlready) {
+        return res.status(400).json({ message: 'User already exist' });
+    }
     const hashedPassword = await userModel.hashPassword(password);
     const user = await userService.createUser({
         firstname: fullname.firstname, 
